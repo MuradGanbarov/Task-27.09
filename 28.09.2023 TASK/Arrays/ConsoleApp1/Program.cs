00759c61959dbd260c57f739fcbb6acac8bba655
MuradGanbarov <145640606+MuradGanbarov@users.noreply.github.com>
2023-09-29 00:14:43 +0400

    Arrays
    Tasks About Arrays

---
+++ 28.09.2023 TASK/Arrays/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿//TASK 1
+//int[] numbers = {1,2,3,4,5};
+//int sum = 0;
+
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    sum += numbers[i];
+
+//}
+//Console.Write(sum);
+
+//TASK 2
+//string[] sentence = { "Murad", "Adil", "Yusif", "Sabir" };
+//for (int i = 0; i < sentence.Length ; i++)
+//{
+//    if (sentence[i].Length > 4)
+//    {
+//        Console.WriteLine(sentence[i]);
+//    }
+//}
+
+//TASK 3
+//int[] M = { 1, 2, 5, 6, 8, 7 };
+//Console.WriteLine("in ascending order");
+//for (int i = 0; i < M.Length; i++)
+//{
+//    Console.WriteLine(M[i]);
+//}
+//Console.WriteLine("in descending order");
+//for (int i = M.Length - 1; i >= 0; i--)
+//{
+//    Console.WriteLine($"{M[i]}");
+//}
