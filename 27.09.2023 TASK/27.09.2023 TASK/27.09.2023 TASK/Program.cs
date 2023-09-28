d7fa4e2cf1fe4ac0271aa62f1f16ae6f44f1eaf4
MuradGanbarov <145640606+MuradGanbarov@users.noreply.github.com>
2023-09-28 04:21:23 +0400

    Task 27.09.2023

---
+++ 27.09.2023 TASK/27.09.2023 TASK/27.09.2023 TASK/Program.cs
@@ -0,0 +1,18 @@
+﻿//TASK1
+//Console.WriteLine("Please write the number which do you want to check:");
+//int number = int.Parse(Console.ReadLine());
+//if (number <= 2)
+//{
+//    Console.WriteLine("Wrong input ");
+//    return;
+//}
+//while (number > 1)
+//{
+//    if (number % 2 != 0)
+//    {
+//        Console.WriteLine("Digit is not degree of 2");
+//        return;
+//    }
+//    number /= 2;
+//}
+//Console.WriteLine("Digit is degree of 2");
